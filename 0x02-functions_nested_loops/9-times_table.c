784936e3245f432170753885f5fa3990bfcc8820
olami928 <oladimejialabi45@gmail.com>
2023-08-18 10:20:52 -0700

    hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,65 +1,73 @@
+
 #include "main.h"
 
-/**
- *
- * * times_table - prints the 9 times table, starting with 0
- *
- * */
 
-void times_table(void)
+
+/**
+
+ * times_table - prints the 9 times table, starting with 0
+ *
+ */
+
+ void times_table(void)
 
 {
 
-	int i, j, k;
+	  int i, j, k;
 
-	for (i = 0; i < 10; i++)
 
-	{
 
-		for (j = 0; j < 10; j++)
+	    for (i = 0; i < 10; i++)
 
-		{
+		      {
 
-			k = j * i;
+			          for (j = 0; j < 10; j++)
 
-			if (j == 0)
+					      {
 
-			{
+						            k = j * i;
 
-				_putchar(k + '0');
+							          if (j == 0)
 
-			}
+									        {
 
-			if (k < 10 && j != 0)
+											        _putchar(k + '0');
 
-			{
+												      }
 
-				_putchar(',');
 
-				_putchar(' ');
 
-				_putchar(' ');
+								        if (k < 10 && j != 0)
 
-				_putchar(k + '0');
+										      {
 
-			} else if (k >= 10)
+											              _putchar(',');
 
-			{
+												              _putchar(' ');
 
-				_putchar(',');
+													              _putchar(' ');
 
-				_putchar(' ');
+														              _putchar(k + '0');
 
-				_putchar((k / 10) + '0');
+															            } else if (k >= 10)
 
-				_putchar((k % 10) + '0');
+																	          {
 
-			}
+																			          _putchar(',');
 
-		}
+																				          _putchar(' ');
 
-		_putchar('\n');
+																					          _putchar((k / 10) + '0');
 
-	}
+																						          _putchar((k % 10) + '0');
 
-sh: 1: q: not found
+																							        }
+
+																        }
+
+				      _putchar('\n');
+
+				        }
+
+}
+
