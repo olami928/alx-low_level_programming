aa968b251e44f7025d74b92cde35c6788b33fab0
olami928 <oladimejialabi45@gmail.com>
2023-08-22 09:31:08 -0700

    hot fix

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98.
+ * @n: Pointer to an integer to be updated.
+ *
+ * This function takes a pointer to an integer and updates the value it
+ * points to with the value 98.
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ this is a new project under this repo
