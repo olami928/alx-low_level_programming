275aca95621d14bf01f6deae53a005226a1fdd4e
olami928 <oladimejialabi45@gmail.com>
2023-08-22 13:52:46 -0700

    hot fix

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @str: The string to be printed.
+ *
+ * Description: This function takes a string as input and prints it
+ *              to the standard output, followed by a new line character.
+ */
+void _puts(char *str)
+{
+
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int main(void)
+{
+	char myString[] = "Hello, world!";
+
+	_puts(myString);
+	return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse, followed by a new line.
+ * @s: The string to be printed in reverse.
+ *
+ * Description: This function takes a string as input and prints its
+ *              characters in reverse order to the standard output,
+ *              followed by a new line character.
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	for (int i = length - 1; i >= 0; i--)
+		putchar(s[i]);
+
+		putchar('\n');
+}
+
+int main(void)
+{
+	char myString[] = "Hello, world!";
+
+	print_rev(myString);
+	return (0);
+}
+
