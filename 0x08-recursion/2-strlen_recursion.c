37343f9c0e64feb867f99688ecf2aecdf43a0371
olami928 <oladimejialabi454@gmail.com>
2023-08-31 16:51:45 -0700

    hot fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-**
+/**
  * _strlen_recursion - Returns Length of String
  * @s: string
  *
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-**
+/**
  * factorial - gets factorial of n
  * @n: integer
  *
