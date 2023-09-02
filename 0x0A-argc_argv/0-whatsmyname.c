d9171b788f187ddb1de4fa03c9ea8043f2f8836b
olami928 <oladimejialabi454@gmail.com>
2023-09-02 08:48:46 -0700

    hot fix

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints its name.
+ * @argv: an array of string containing the argument
+ * @argc: number of commaND line
+ * return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0 && argv[] != NULL)
+		printf("The name of the program is : %s\n", argv[0]);
+	else
+		printf("The name of the program could not be determined\n");
+	return (0);
+}
