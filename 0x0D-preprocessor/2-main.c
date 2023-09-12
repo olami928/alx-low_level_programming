08e2d14667f88b824d456f34a442b02598e5f344
olami928 <oladimejialabi454@gmail.com>
2023-09-12 08:25:58 -0700

    hot fix

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main
+ * @__FILE__: returnsa the name of the file.
+ * return(0)
+ */
+
+int main()
+{
+	printf("The name of this file is %s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,12 @@
+// C program to print the name of
+// source code file
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Name of source file: %s\n", __FILE__);
+
+    return 0;
+}
+
