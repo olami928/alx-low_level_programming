0e110d0afdd94be1cc95afc93462f2989c400154
olami928 <oladimejialabi454@gmail.com>
2023-09-12 15:55:53 -0700

    hot fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * main
- * @__FILE__: returnsa the name of the file.
+ * main - print name of file compiled from
+ *
  * return(0)
  */
 
-int main()
+int main(void)
 {
 	printf(" %s\n", __BASE_FILE__);
 	return (0);
