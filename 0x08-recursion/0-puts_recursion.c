68407bff174b696302da84e59740bbb0bdb20089
olami928 <oladimejialabi45@gmail.com>
2023-09-28 17:12:45 +0100

    hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,8 +4,7 @@
  * projrct - recursion
  */
 /**
- *  _puts_recursion - Prints a string followed by a new line
- *  @s: string
+ *  _puts_recursion - function like puts()
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -13,9 +12,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
 		_putchar('\n');
-		return;
-	}								}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
