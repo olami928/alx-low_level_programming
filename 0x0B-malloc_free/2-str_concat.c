3224b7c76afad6db63aecbc1e138989d248abc66
olami928 <oladimejialabi45@gmail.com>
2023-10-06 09:10:53 +0100

    hot fix

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * str_concat - concatenates two strings s1 and s2
+ * @s1: memory address of pointer
+ * @s2: memory address of pointer
+ * return (0); Always
+ */
+char *str_concat(char *s1, char *s2)
+{
+	
+}
