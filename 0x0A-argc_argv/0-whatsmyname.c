f8a1fe259947ee8c48a2df0835aac84feb705b16
olami928 <oladimejialabi45@gmail.com>
2023-10-07 14:25:19 +0100

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - prints name of program
+ * main - prints name of program, followed by a new line..
  * @argc: argument count.
  * @argv: argument vector.
- * return 0;
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
