382ed20ea5a55d610b435948a65392e047392d21
olami928 <oladimejialabi45@gmail.com>
2023-10-15 15:57:41 +0100

    hot fix

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+ * op_add - addition operation
+ * @a: memory address
+ * @b: memory address
+ * Return: 0 on success
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction operation
+ * @a: memory address
+ * @b: memory address
+ * Return: 0 on success
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication operation
+ * @a: memory address
+ * @b: memory address
+ * Return: 0 on success
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division operation
+ * @a: memory address
+ * @b: memory address
+ * Return: 0 on success
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100)
+	}
+	return (a / b);
+}
+/**
+ * op_mod - modular operation
+ * @a: memory address
+ * @b: memory address
+ * Return: 0 on succes
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
